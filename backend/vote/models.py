ea185ff7177338137fcc47610e54286b46a52d0b
Coderyounes <younesanas2021@gmail.com>
2024-02-18 23:40:44 +0100

    fix(vote): import review.model

--- backend/vote/models.py
+++ backend/vote/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from backend.review.models import Review
+from review.models import Review
 from django.utils import timezone
 # Create your models here.
 
