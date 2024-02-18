279cf14d98f7c4811f0dfa4bffa4ac58a7997dc0
Coderyounes <younesanas2021@gmail.com>
2024-02-18 22:25:12 +0100

    feat(book model) :  add new fields to Book Model

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from user.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=100, null=False, unique=True, blank=False)
+    title = models.CharField(max_length=100, null=False, unique=True, blank=False)
     description = models.TextField()
     author = models.CharField(max_length=100, null=False, unique=True, blank=False)
+    publication_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_url = models.URLField()
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.title
