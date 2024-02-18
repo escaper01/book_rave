e59598acdf71c2ccd43092d9f1d0910994a52ace
Coderyounes <younesanas2021@gmail.com>
2024-02-18 23:21:15 +0100

    fix(): set  cover field for book & add default value None

--- backend/book/models.py
+++ backend/book/models.py
@@ -7,13 +7,13 @@ class Book(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True, blank=False)
     description = models.TextField()
     author = models.CharField(max_length=100, null=False, unique=True, blank=False)
-    publication_date = models.DateField()
+    publication_date = models.DateField(null=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     # in View Create_model , make sure to extract the request first_name & last_name
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     # Will Be Updated to handle S3 Bucket Storage
-    image_url = models.URLField()
+    cover = models.ImageField(upload_to="uploads/books/%Y/%m/%d/", default=None)
 
     class Meta:
         pass
