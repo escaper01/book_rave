10d18e4ee5bb91dd34e4a7fd78f32c692d1718d4
Coderyounes <younesanas2021@gmail.com>
2024-02-25 00:34:35 +0100

    fix(): handle case where noid is supplied in get-review

--- backend/review/urls.py
+++ backend/review/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('v1/review/<id>', getreview, name='getreview'),
     path('v1/book/<id>/review', allreview, name='allreview'),
     path('v1/book/review', allreview_noid, name='allreview_noid'),
+    path('v1/review/', allreview_noid, name='allreview_noid'),
 ]
