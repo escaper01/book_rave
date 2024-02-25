906fbd3a5e45b3a16cf248bda8b9e4d914244be7
Coderyounes <younesanas2021@gmail.com>
2024-02-25 19:33:25 +0100

    minor: contineu to setup boilerplate for comment model

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import getcomments, getcomment, removecomment
+from .views import getcomments, getcomment, removecomment, addcomment, updatecomment
 
 urlpatterns = [
     path('v1/GetComment', getcomments, name='getcomments'),
     path('v1/getcomment/<id>', getcomment, name='getcomment'),
     path('v/deletecomment', removecomment, name='removecomment'),
+    path('v1/addcomment', addcomment, name='addcomment'),
+    path('v1/updatecomment', updatecomment, name='updatecomment'),
 ]
