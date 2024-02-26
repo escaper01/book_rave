06ddb9116aa831241c12a4d453f788b1f50f5ac2
Coderyounes <younesanas2021@gmail.com>
2024-02-26 03:46:01 +0100

    prepare the basic vote view for tomorrow

--- backend/vote/views.py
+++ backend/vote/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def like_view(request, pk):
+    vote = get_object_or_404(Vote, pk=pk)
+    vote.vote_type = 'UP'
+    vote.save()
+    return JsonResponse({'message': 'Vote updated successfully'})
+
+def dislike_view(request, pk):
+    vote = get_object_or_404(Vote, pk=pk)
+    vote.vote_type = 'DOWN'
+    vote.save()
+    return JsonResponse({'message': 'Vote updated successfully'})
