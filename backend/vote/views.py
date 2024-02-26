ca70c61e9597fd974d59e064807a22938a487732
Coderyounes <younesanas2021@gmail.com>
2024-02-26 18:13:03 +0100

    update my branch with auth features

--- backend/vote/views.py
+++ backend/vote/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def like_view(request, pk):
+"""def like_view(request, pk):
     vote = get_object_or_404(Vote, pk=pk)
     vote.vote_type = 'UP'
     vote.save()
@@ -13,4 +13,4 @@ def dislike_view(request, pk):
     vote = get_object_or_404(Vote, pk=pk)
     vote.vote_type = 'DOWN'
     vote.save()
-    return JsonResponse({'message': 'Vote updated successfully'})
+    return JsonResponse({'message': 'Vote updated successfully'})"""
