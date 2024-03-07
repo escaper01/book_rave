d47cb5a091165c57e1fcc446d084341239d904d0
Coderyounes <younesanas2021@gmail.com>
2024-03-07 23:01:02 +0100

    update(): refactor the Review views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include("user.urls")),
     path('api/v1/book/', include("book.urls")),
-    path('api/v1/review', include("review.urls")),
+    path('api/v1/review/', include("review.urls")),
     path('api/v1/comment', include("comment.urls")),
     re_path('api/v1/auth/', include('djoser.urls')),
     re_path('api/v1/auth/', include('djoser.urls.jwt')),
--- backend/review/serializers.py
+++ backend/review/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Review
+from book.models import Book
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = "__all__"
+        fields = ['title', 'media', 'content', 'rating', 'book']
