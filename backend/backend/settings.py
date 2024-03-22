c4d0ea5278f12a57c55b2c37ea4b63875c248c41
escaper <assaoui54@gmail.com>
2024-03-22 23:10:38 +0000

    removed media folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -170,8 +170,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-# and renames the files with unique names for each version to support long-term caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS_ALLOWED_ORIGINS = [
