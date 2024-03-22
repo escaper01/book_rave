542323eab8502edebb8e8ac73e98de87cceae9be
escaper <assaoui54@gmail.com>
2024-03-22 21:10:58 +0000

    migrate fix

--- backend/build.sh
+++ backend/build.sh
@@ -10,4 +10,4 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --run-syncdb
