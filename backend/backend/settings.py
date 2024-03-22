80f4a00a29639db7ec8dc5629dc0de5a0ee02281
escaper <assaoui54@gmail.com>
2024-03-22 21:49:20 +0000

    add slash for media path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -166,7 +166,7 @@ DJOSER = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
