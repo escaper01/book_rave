5a158ca01354206a3de42a1ff371c86defe4eb48
escaper <assaoui54@gmail.com>
2024-03-22 21:37:00 +0000

    fix:edited url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
 
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEUBG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
