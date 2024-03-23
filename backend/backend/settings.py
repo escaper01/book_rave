8c2a8e9c38e6bf55e01bde9ea742791f6b0a3375
escaper <assaoui54@gmail.com>
2024-03-23 15:56:21 +0000

    added slash

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -203,4 +203,4 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
