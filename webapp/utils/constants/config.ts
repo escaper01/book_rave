5c30d43c7343b89180f415c81eabb2491b421fde
escaper <assaoui54@gmail.com>
2024-03-23 17:55:54 +0000

    s3 final tweak

--- webapp/utils/constants/config.ts
+++ webapp/utils/constants/config.ts
@@ -2,7 +2,7 @@ import { LogoutPayloadType } from '../types/AuthTypes';
 import { ProfileFormType } from '../types/ProfileType';
 
 // export const BASE_URL = 'http://localhost:8000/api/v1';
-// export const BASE_URL = 'https://localhost:8000/api/v1';
+// export const BASE_URL = 'http://localhost:8000/api/v1';
 export const BASE_URL = 'https://book-rave.onrender.com/api/v1';
 
 export const MAX_FILE_SIZE = 500000;
